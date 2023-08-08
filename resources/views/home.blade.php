b6b3c1c7ba5dade48a2ca1dfab0f087915257a9b
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-08 09:46:44 +0900

    boostrap入れた（トップページ）

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Game Selection</title>
-</head>
-<body>
-    <h1>Welcome to the Games!</h1>
-    <div>
-        <h2>Select a Game</h2>
-        <a href="{{ route('questions.index') }}">ウミガメのスープ</a><br>
-        <a href="#">自己紹介嘘当てゲーム (Coming Soon)</a>
-    </div>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+<h1>Welcome to the Games!</h1>
+<div>
+    <h2>Select a Game</h2>
+    <a href="{{ route('questions.index') }}" class="btn btn-primary">ウミガメのスープ</a><br>
+    <a href="#" class="btn btn-secondary mt-2">自己紹介嘘当てゲーム (Coming Soon)</a>
+</div>
+@endsection
