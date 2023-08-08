36e0f7cda3ba36078b8191d2176c8c71600e3619
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-08 09:27:07 +0900

    トップページHTMLのみ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,14 @@
-<!-- resources/views/home.blade.php -->
-<h1>Home Page</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Selection</title>
+</head>
+<body>
+    <h1>Welcome to the Games!</h1>
+    <div>
+        <h2>Select a Game</h2>
+        <a href="{{ route('questions.index') }}">ウミガメのスープ</a><br>
+        <a href="#">自己紹介嘘当てゲーム (Coming Soon)</a>
+    </div>
+</body>
+</html>
