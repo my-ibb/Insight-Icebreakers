72aac3b6049ea58abc389626a6c3e80b64600642
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 01:36:33 +0900

    トップページ　背景色やボタン等変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,49 @@
 @section('title', 'Home Page')
 
 @section('content')
-<h1>Welcome to the Games!</h1>
-<div>
-    <h2>Select a Game</h2>
-    <a href="{{ route('questions.index') }}" class="btn btn-primary">ウミガメのスープ</a><br>
-    <a href="#" class="btn btn-secondary mt-2">自己紹介嘘当てゲーム (Coming Soon)</a>
 
+<!-- カスタムCSSスタイル -->
+<style>
+    /* 背景色とヘッダーのスタイル */
+    body {
+        background-color: lightblue;
+    }
+    .header-title {
+        color: blue;
+        font-weight: bold;
+    }
+    /* ボタンのスタイル */
+    .turtle-btn {
+        background-color: lightgreen;
+    }
+    .lie-btn {
+        background-color: lightyellow;
+    }
+    .custom-btn {
+        width: 200px;
+        height: 200px;
+    }
+</style>
+
+<h1 class="header-title text-center mt-5">Insight Icebreakers</h1>
+<h2 class="text-center">Welcome to the Games!</h2>
+<div class="container text-center mt-4">
+    <!-- ゲーム選択ボタン -->
+    <h2>Select a Game</h2>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-4">
+            <a href="{{ route('questions.index') }}" class="btn turtle-btn custom-btn">ウミガメのスープ</a>
+        </div>
+        <div class="col-md-4">
+            <a href="#" class="btn lie-btn custom-btn">自己紹介嘘当てゲーム (Coming Soon)</a>
+        </div>
+    </div>
+
+    <!-- ログアウトボタン -->
     @if($isLoggedIn)
-    <form method="POST" action="{{ route('logout') }}" class="mt-2">
+    <form method="POST" action="{{ route('logout') }}" class="mt-4">
         @csrf
-        <button type="submit">Logout</button>
+        <button type="submit" class="btn btn-danger">Logout</button>
     </form>
     @endif
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('styles.css') }}">
-    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
-<body>
+{{-- <body class="bg $cyan-300"> --}}
+    
     <header>
 
     </header>
@@ -20,8 +22,5 @@
         <!-- Footer here -->
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
