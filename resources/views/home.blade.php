8f3b73579507e7700733e99f4bdf4cee420f6111
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 01:56:35 +0900

    ログアウトボタン修正とpublicディレクトリ戻す前

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('title', 'Home Page')
-
 @section('content')
 
 <!-- カスタムCSSスタイル -->
@@ -34,19 +33,28 @@
     <h2>Select a Game</h2>
     <div class="row justify-content-center mt-5">
         <div class="col-md-4">
-            <a href="{{ route('questions.index') }}" class="btn turtle-btn custom-btn">ウミガメのスープ</a>
+            <a href="{{ route('questions.index') }}" class="btn turtle-btn custom-btn">
+                <!-- ウミガメのスープのイラスト追加 -->
+                <img src="{{ asset('images/23048191.jpg') }}" alt="ウミガメのスープ" class="img-fluid">
+                ウミガメのスープ
+            </a>
         </div>
         <div class="col-md-4">
-            <a href="#" class="btn lie-btn custom-btn">自己紹介嘘当てゲーム (Coming Soon)</a>
+            <a href="#" class="btn lie-btn custom-btn">
+                <!-- 自己紹介嘘当てゲームのイラスト追加 -->
+                <img src="{{ asset('images/24220784.jpg') }}" alt="自己紹介嘘当てゲーム" class="img-fluid">
+                自己紹介嘘当てゲーム (Coming Soon)
+            </a>
         </div>
     </div>
-
-    <!-- ログアウトボタン -->
-    @if($isLoggedIn)
-    <form method="POST" action="{{ route('logout') }}" class="mt-4">
-        @csrf
-        <button type="submit" class="btn btn-danger">Logout</button>
-    </form>
+    
+    @if(isset($isLoggedIn) && $isLoggedIn)
+    <div class="mt-4">
+        <form method="POST" action="{{ route('logout') }}" class="mt-2">
+            @csrf
+            <button type="submit" class="btn btn-danger">Logout</button>
+        </form>
+    </div>
     @endif
 </div>
 @endsection
