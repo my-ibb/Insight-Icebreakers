3aa0034096e345aeb3332ae8b569048a5409505e
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 21:13:44 +0900

    マイページデザイン修正

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,12 +1,41 @@
+<!-- resources/views/mypage.blade.php -->
 @extends('layouts.app')
 
-@section('title', 'My Page')
+@section('title', 'マイページ')
 
 @section('content')
 
-<!-- resources/views/mypage.blade.php -->
+<div class="container my-5">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="display-4">My page</h1>
+        </div>
+    </div>
 
-<h1>My Page</h1>
+    <div class="row mt-4">
+        <div class="col-3">
+            <div class="card">
+                <div class="card-header">
+                    ユーザー情報
+                </div>
+                <div class="card-body">
+                    <p class="card-text">名前: （ユーザー名）</p>
+                    <p class="card-text">メール: （メールアドレス）</p>
+                </div>
+            </div>
+        </div>
 
-<p>Welcome to my page!</p>
+        <div class="col-9">
+            <div class="card">
+                <div class="card-header">
+                    アクティビティ
+                </div>
+                <div class="card-body">
+                    <p class="card-text">こちらには最近解いた問題や作成した問題のリストが表示されます。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
