c99799786c18247d1beb5788d3ef0decb66096bf
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 21:23:10 +0900

    生成された質問を別の新しいページで表示のページデザイン修正

--- resources/views/questions/generated.blade.php
+++ resources/views/questions/generated.blade.php
@@ -4,19 +4,20 @@
 
 @section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generated Question</title>
-</head>
-<body>
-    <h2>Generated Question:</h2>
-    <p>{{ $question }}</p>
-    <a href="{{ route('questions.create') }}">Back to Create Question</a>
-</body>
-</html>
-@endsection
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 center-div">
+            <div class="card">
+                <div class="card-header bg-light text-dark">
+                    <h2>Generated Question:</h2>
+                </div>
+                <div class="card-body">
+                    <p>{{ $question }}</p>
+                    <a href="{{ route('questions.create') }}" class="btn btn-primary">Back to Create Question</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+@endsection
