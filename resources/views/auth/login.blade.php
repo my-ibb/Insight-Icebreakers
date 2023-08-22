29a131122dcab3b1009cde92acb346becbe78455
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 21:20:02 +0900

    ログインフォーム真ん中に寄せた

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,39 @@
-<!-- resources/views/auth/login.blade.php -->
 @extends('layouts.app')
 
 @section('title', 'Login Page')
 
 @section('content')
 
-<h1 class="text-center mb-4">Login Form</h1>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <!-- エラーメッセージの表示 -->
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    @foreach ($errors->all() as $error)
-                        <p class="mb-0">{{ $error }}</p>
-                    @endforeach
-                </div>
-            @endif
-            
-            <!-- ログインフォームのHTML -->
-            <form method="post" action="/login">
-                @csrf
-                
-                <div class="form-group">
-                    <label for="email">メールアドレス:</label>
-                    <input type="text" id="email" name="email" class="form-control" required>
-                </div>
-                
-                <div class="form-group">
-                    <label for="password">パスワード:</label>
-                    <input type="password" id="password" name="password" class="form-control" required>
-                </div>
-                
-                <div class="form-group text-center">
-                    <input type="submit" value="ログイン" class="btn btn-primary">
-                </div>
-            </form>
-            <p class="text-center">
-                <a href="{{ url('/register') }}">新規登録</a>
-            </p>
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="text-center">ログイン</h1>
+            </div>
+            <div class="card-body">
+                <!-- エラーメッセージの表示 -->
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                @endif
+                <form method="post" action="/login">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">メールアドレス:</label>
+                        <input type="text" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">パスワード:</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="text-center">
+                        <input type="submit" class="btn btn-primary" value="ログイン">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
