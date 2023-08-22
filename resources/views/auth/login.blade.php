c34a871f1e05126b6aa2ad253fc63836099059a5
my-ibb <hayashimai0302@yahoo.co.jp>
2023-08-22 21:04:57 +0900

    ログインフォームデザイン修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,31 +5,43 @@
 
 @section('content')
 
-<h1>Login Form</h1>
+<h1 class="text-center mb-4">Login Form</h1>
 
-<!-- エラーメッセージの表示 -->
-@if ($errors->any())
-    <div>
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- エラーメッセージの表示 -->
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <p class="mb-0">{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
+            
+            <!-- ログインフォームのHTML -->
+            <form method="post" action="/login">
+                @csrf
+                
+                <div class="form-group">
+                    <label for="email">メールアドレス:</label>
+                    <input type="text" id="email" name="email" class="form-control" required>
+                </div>
+                
+                <div class="form-group">
+                    <label for="password">パスワード:</label>
+                    <input type="password" id="password" name="password" class="form-control" required>
+                </div>
+                
+                <div class="form-group text-center">
+                    <input type="submit" value="ログイン" class="btn btn-primary">
+                </div>
+            </form>
+            <p class="text-center">
+                <a href="{{ url('/register') }}">新規登録</a>
+            </p>
+        </div>
     </div>
-@endif
-
-<!-- ログインフォームのHTML -->
-<form method="post" action="/login">
-    @csrf
-
-    <label for="email">メールアドレス:</label>
-    <input type="text" id="email" name="email" required>
-    <br>
-    <label for="password">パスワード:</label>
-    <input type="password" id="password" name="password" required>
-    <br>
-    <input type="submit" value="ログイン">
-</form>  
-<p>
-    <a href="{{ url('/register') }}">新規登録</a>
-</p>
+</div>
 
 @endsection
