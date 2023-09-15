916a594ad1e0d2143e5d34e7ffb79b04ef027430
my-ibb <hayashimai0302@yahoo.co.jp>
2023-09-15 23:49:34 +0900

    プレイヤー設定画面

--- resources/views/resources/self_introduction_lie_game.blade.php
+++ resources/views/resources/self_introduction_lie_game.blade.php
@@ -1,24 +1,40 @@
-<form action="{{ route('selfIntroductionLieGame.start') }}" method="post">
-    @csrf
-    <div>
-        <label for="player_name">プレイヤー名:</label>
-        <input type="text" id="player_name" name="player_name" required>
-    </div>
-    <div>
-        <label for="number_of_players">参加人数:</label>
-        <select id="number_of_players" name="number_of_players">
-            @foreach($numberOfPlayersOptions as $option)
-                <option value="{{ $option }}">{{ $option }}人</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <label for="number_of_questions">設問個数:</label>
-        <select id="number_of_questions" name="number_of_questions">
-            @foreach($numberOfQuestionsOptions as $option)
-                <option value="{{ $option }}">{{ $option }}個</option>
-            @endforeach
-        </select>
-    </div>
-    <button type="submit">開始</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="text-center">プレイヤー設定画面</h1>
+
+    <form action="{{ route('selfIntroductionLieGame.start') }}" method="post" class="mt-5">
+        @csrf
+
+        <!-- プレイヤー名入力 -->
+        <div class="form-group">
+            <label for="player_name">プレイヤー名</label>
+            <input type="text" class="form-control" id="player_name" name="player_name">
+        </div>
+
+        <!-- 参加人数選択 -->
+        <div class="form-group">
+            <label for="number_of_players">参加人数</label>
+            <select class="form-control" id="number_of_players" name="number_of_players">
+                @foreach ($numberOfPlayersOptions as $option)
+                    <option value="{{ $option }}">{{ $option }}人</option>
+                @endforeach
+            </select>
+        </div>
+
+        <!-- 設問数選択 -->
+        <div class="form-group">
+            <label for="number_of_questions">設問数</label>
+            <select class="form-control" id="number_of_questions" name="number_of_questions">
+                @foreach ($numberOfQuestionsOptions as $option)
+                    <option value="{{ $option }}">{{ $option }}問</option>
+                @endforeach
+            </select>
+        </div>
+
+        <!-- 送信ボタン -->
+        <button type="submit" class="btn btn-primary">開始</button>
+    </form>
+</div>
+@endsection
