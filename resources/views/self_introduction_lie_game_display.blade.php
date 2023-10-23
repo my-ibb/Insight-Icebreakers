4b609dde4360f0291331d4f2dd239a911c1aceae
my-ibb <hayashimai0302@yahoo.co.jp>
2023-10-24 00:41:47 +0900

    自己紹介文表示と非表示できるようにした

--- resources/views/self_introduction_lie_game_display.blade.php
+++ resources/views/self_introduction_lie_game_display.blade.php
@@ -4,9 +4,12 @@
 @section('content')
 <div class="container">
     <h1 class="text-center">自己紹介文表示</h1>
-    @foreach($summaries as $summary)
+    @foreach($summaries as $index => $summary)
         <h2>{{ $summary['player_name'] }}</h2>
-        <p>{{ $summary['summary'] }}</p>
+        <div style="display:none;" id="answerArea-{{ $index }}">
+            <p>{{ $summary['summary'] }}</p>
+        </div>
+        <button onclick="showAnswer({{ $index }})">表示/非表示</button>
     @endforeach
 
     {{-- トップページに戻るボタンを追加 --}}
@@ -15,4 +18,15 @@
         <button type="submit" class="btn btn-primary">トップページに戻る</button>
     </form>
 </div>
+
+<script>
+    function showAnswer(id) {
+        var answerArea = document.getElementById('answerArea-' + id);
+        if (answerArea.style.display === 'block') {
+            answerArea.style.display = 'none';
+        } else {
+            answerArea.style.display = 'block';
+        }
+    }
+</script>
 @endsection
